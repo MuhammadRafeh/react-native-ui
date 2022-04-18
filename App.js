62a15d08b252ed2eb17bdc9e7cd1888507b58768
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-04-18 21:19:40 +0500

    Added New Files and with installed packages!

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
+}
+
+export default App;
