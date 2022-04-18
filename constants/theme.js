f55ceee576072df88f9870c2a521f706d40dd9fd
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-04-19 02:24:35 +0500

    Placed mostly Things!

---
+++ constants/theme.js
@@ -0,0 +1,5 @@
+export default {
+    primary: '#226AC6',
+    secondary: '#351E76',
+    tertairy: '#4D2384'
+}
--- screens/auth/SignUpScreen.js
+++ screens/auth/SignUpScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import BigSun from '../../assets/images/signup/bigSun.svg';
+import RunSun from '../../assets/images/signup/runSun.svg';
+import Sun from '../../assets/images/signup/sun.svg';
+import LinearGradient from 'react-native-linear-gradient';
+import theme from '../../constants/theme';
 
 const SignUpScreen = props => {
     return (
-        <View style={styles.screen}>
-            <BigSun width={120} height={40} fill={"black"} />
-        </View>
+        <>
+            <LinearGradient colors={[theme.primary, theme.secondary, theme.secondary, theme.tertairy, theme.tertairy]} style={styles.linearGradient} />
+            <View style={{ top: 70, left: 71, position: 'absolute' }}>
+                <BigSun width={51} height={51} fill={"black"} />
+            </View>
+            <View style={{ top: 145, right: -4, position: 'absolute' }}>
+                <RunSun width={117} height={84} fill={"black"} />
+            </View>
+            <View style={{ right: '-5.7%', bottom: '18%', position: 'absolute' }}>
+                <Sun width={117} height={84} fill={"black"} />
+            </View>
+            <View style={styles.screen}>
+            </View>
+        </>
     );
 }
 
@@ -14,7 +29,13 @@ export default SignUpScreen;
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1,
-        backgroundColor: 'blue',
+        flex: 1
+    },
+    linearGradient: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
     }
 })
